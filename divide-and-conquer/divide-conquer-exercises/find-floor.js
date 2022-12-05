2019c4474cb519a74be7eada21de5e19c298eb43
Jacob Eiferman <jacobeiferman@gmail.com>
2022-12-05 15:09:41 -0500

    find floor

--- divide-and-conquer/divide-conquer-exercises/find-floor.js
+++ divide-and-conquer/divide-conquer-exercises/find-floor.js
@@ -1,5 +1,14 @@
-function findFloor() {
-  
+function findFloor(arr, num) {
+	let start = 0;
+	let end = arr.length - 1;
+	while (start <= end) {
+		let mid = Math.floor((start + end) / 2);
+		if (mid === arr.length - 1 && arr[mid] <= num) return arr[mid];
+		if (arr[mid] <= num && arr[mid + 1] >= num) return arr[mid];
+		else if (arr[mid] <= num && arr[mid + 1] <= num) start = mid + 1;
+		else end = mid - 1;
+	}
+	return -1;
 }
 
-module.exports = findFloor
+module.exports = findFloor;
