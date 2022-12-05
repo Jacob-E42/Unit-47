10d4a9ba21d349af7450130cea3e19d20b095664
Jacob Eiferman <jacobeiferman@gmail.com>
2022-12-05 14:37:19 -0500

    find rotated index

--- divide-and-conquer/divide-conquer-exercises/find-rotated-index.js
+++ divide-and-conquer/divide-conquer-exercises/find-rotated-index.js
@@ -1,40 +1,32 @@
 function findRotatedIndex(arr, num) {
 	let firstRotatedNumberIndex = findFirstRotatedNumberIndex(arr);
-	console.log(arr, "\n", "first rotated:", arr[firstRotatedNumberIndex]);
-	if (firstRotatedNumberIndex === 0) return findIndex(arr, num);
-	let arrSegmentStartingIndex = findArrSegment(arr, num, firstRotatedNumberIndex);
-	if (arrSegmentStartingIndex === -1) return -1;
-	let arrSegmentStoppingIndex = arrSegmentStartingIndex === 0 ? firstRotatedNumberIndex - 1 : arr.length - 1;
-	console.log("start:", arrSegmentStartingIndex, "stop:", arrSegmentStoppingIndex);
-	return findIndex(arr, num, arrSegmentStartingIndex, arrSegmentStoppingIndex);
+	if (firstRotatedNumberIndex > 0 && num >= arr[0] && num <= arr[firstRotatedNumberIndex - 1]) {
+		return findIndex(arr, num, 0, firstRotatedNumberIndex - 1);
+	} else {
+		return findIndex(arr, num, firstRotatedNumberIndex, arr.length - 1);
+	}
 }
 
 function findFirstRotatedNumberIndex(arr, left = 0, right = arr.length - 1) {
 	if (arr.length === 1 || arr[0] < arr[arr.length - 1]) return 0;
 	if (left <= right) {
-		let mid = left + Math.floor((right - left) / 2);
-		console.log(arr, "\n", "left:", arr[left], "mid:", arr[mid], "right:", arr[right]);
+		let mid = Math.floor((right + left) / 2);
+		if (arr[mid] < arr[mid - 1]) return mid;
+		else if (arr[mid] >= arr[right]) return findFirstRotatedNumberIndex(arr, mid + 1, right);
+		else return findFirstRotatedNumberIndex(arr, left, mid - 1);
 	}
-	return -1;
 }
 
-function findArrSegment(arr, num, firstRotated) {
-	let rightArrHighIndex = arr.length - 1;
-	let leftArrHighIndex = firstRotated - 1;
-	if (num < arr[rightArrHighIndex]) return firstRotated;
-	else if (num <= arr[leftArrHighIndex]) return 0;
-	return -1;
-}
-
-function findIndex(arr, num, left = 0, right = arr.length - 1) {
+function findIndex(arr, num, left, right) {
+	if (arr.length === 0) return -1;
+	if (num < arr[left] || num > arr[right]) return -1;
 	if (left <= right) {
-		let mid = left + Math.floor((right - left) / 2);
-		console.log(arr, "\n", "left:", arr[left], "mid:", arr[mid], "right:", arr[right]);
-		if (num < arr[mid]) {
-			return findIndex(arr, num, left, right - 1);
+		let mid = Math.floor((right + left) / 2);
+		if (arr[mid] === num) {
+			return mid;
 		} else if (arr[mid] < num) {
 			return findIndex(arr, num, left + 1, right);
-		} else return mid;
+		} else return findIndex(arr, num, left, right - 1);
 	}
 	return -1;
 }
