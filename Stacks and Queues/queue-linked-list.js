9363a9cb00ce6b99ba0c61a484d866e26def28fe
Jacob Eiferman <jacobeiferman@gmail.com>
2022-12-07 11:16:00 -0500

    implemented Queue class using LinkedList class

--- Stacks and Queues/queue-linked-list.js
+++ Stacks and Queues/queue-linked-list.js
@@ -43,7 +43,7 @@ class Queue {
 	/** peek(): return the value of the first node in the queue. */
 
 	peek() {
-		return this._list.getAt(0);
+		return this.first.val;
 	}
 
 	/** isEmpty(): return true if the queue is empty, otherwise false */
