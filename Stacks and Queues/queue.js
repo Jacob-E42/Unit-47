7272d2513248a6cd3a1640506b93e527ea2fb18c
Jacob Eiferman <jacobeiferman@gmail.com>
2022-12-07 10:51:02 -0500

    Queue class using LikedList class

--- Stacks and Queues/queue.js
+++ Stacks and Queues/queue.js
@@ -1,46 +1,56 @@
 /** Node: node for a queue. */
+const LinkedList = require("./linked-list");
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
 }
 
 /** Queue: chained-together nodes where you can
  *  remove from the front or add to the back. */
 
 class Queue {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+	constructor() {
+		this._list = new LinkedList();
+		this.first = this._list.head;
+		this.last = this._list.tail;
+		this.size = 0;
+	}
 
-  /** enqueue(val): add new value to end of the queue. Returns undefined. */
+	/** enqueue(val): add new value to end of the queue. Returns undefined. */
 
-  enqueue(val) {
+	enqueue(val) {
+		this._list.push(val);
+		this.first = this._list.head;
+		this.last = this._list.tail;
+		this.size++;
+	}
 
-  }
+	/** dequeue(): remove the node from the start of the queue
+	 * and return its value. Should throw an error if the queue is empty. */
 
-  /** dequeue(): remove the node from the start of the queue
-   * and return its value. Should throw an error if the queue is empty. */
+	dequeue() {
+		if (this.isEmpty()) throw new Error("Error: Queue is empty.");
+		const removed = this._list.shift();
+		this.first = this._list.head;
+		this.last = this._list.tail;
+		this.size--;
+		return removed;
+	}
 
-  dequeue() {
+	/** peek(): return the value of the first node in the queue. */
 
-  }
+	peek() {
+		if (this.size > 0) return this._list.getAt(0);
+	}
 
-  /** peek(): return the value of the first node in the queue. */
+	/** isEmpty(): return true if the queue is empty, otherwise false */
 
-  peek() {
-
-  }
-
-  /** isEmpty(): return true if the queue is empty, otherwise false */
-
-  isEmpty() {
-
-  }
+	isEmpty() {
+		return this._list.length === 0 ? true : false;
+	}
 }
 
 module.exports = Queue;
