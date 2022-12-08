5166f3a02eab2fcd70f42b2e911e7fab946573e0
Jacob Eiferman <jacobeiferman@gmail.com>
2022-12-08 11:44:27 -0500

    revString()

--- Recursion/recursion.js
+++ Recursion/recursion.js
@@ -40,7 +40,10 @@ function findIndex(arr, val, i = 0) {
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {}
+function revString(str) {
+	if (str.length < 1) return "";
+	return str.slice(-1) + revString(str.slice(0, -1));
+}
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
