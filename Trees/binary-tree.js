91785da78790842ca647852d7ac3a65f10610b76
Jacob Eiferman <jacobeiferman@gmail.com>
2022-12-09 10:57:03 -0500

    created minDepth Binary Tree function

--- Trees/binary-tree.js
+++ Trees/binary-tree.js
@@ -1,75 +1,76 @@
 /** BinaryTreeNode: node for a general tree. */
 
 class BinaryTreeNode {
-  constructor(val, left = null, right = null) {
-    this.val = val;
-    this.left = left;
-    this.right = right;
-  }
+	constructor(val, left = null, right = null) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+	childrenAreNull() {
+		return this.left === null && this.right === null;
+	}
 }
 
 class BinaryTree {
-  constructor(root = null) {
-    this.root = root;
-  }
+	constructor(root = null) {
+		this.root = root;
+	}
 
-  /** minDepth(): return the minimum depth of the tree -- that is,
-   * the length of the shortest path from the root to a leaf. */
+	/** minDepth(): return the minimum depth of the tree -- that is,
+	 * the length of the shortest path from the root to a leaf. */
 
-  minDepth() {
+	minDepth() {
+		// toVisit functions like a Queue to keep track of traversed nodes in a Breadth First approach
+		const toVisit = [this.root];
+		let lowestDepth = 0;
+		while (toVisit.length && this.root) {
+			lowestDepth++;
+			let current = toVisit.shift();
+			if (current.childrenAreNull()) return lowestDepth;
+			toVisit.push(current.left);
+			toVisit.push(current.right);
+		}
+		return lowestDepth;
+	}
 
-  }
+	/** maxDepth(): return the maximum depth of the tree -- that is,
+	 * the length of the longest path from the root to a leaf. */
 
-  /** maxDepth(): return the maximum depth of the tree -- that is,
-   * the length of the longest path from the root to a leaf. */
+	maxDepth() {
+		if (!this.root) return 0;
+	}
 
-  maxDepth() {
+	/** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
+	 * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
-  }
+	maxSum() {}
 
-  /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
-   * The path doesn't need to start at the root, but you can't visit a node more than once. */
+	/** nextLarger(lowerBound): return the smallest value in the tree
+	 * which is larger than lowerBound. Return null if no such value exists. */
 
-  maxSum() {
+	nextLarger(lowerBound) {}
 
-  }
+	/** Further study!
+	 * areCousins(node1, node2): determine whether two nodes are cousins
+	 * (i.e. are at the same level but have different parents. ) */
 
-  /** nextLarger(lowerBound): return the smallest value in the tree
-   * which is larger than lowerBound. Return null if no such value exists. */
+	areCousins(node1, node2) {}
 
-  nextLarger(lowerBound) {
+	/** Further study!
+	 * serialize(tree): serialize the BinaryTree object tree into a string. */
 
-  }
+	static serialize() {}
 
-  /** Further study!
-   * areCousins(node1, node2): determine whether two nodes are cousins
-   * (i.e. are at the same level but have different parents. ) */
+	/** Further study!
+	 * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
 
-  areCousins(node1, node2) {
+	static deserialize() {}
 
-  }
+	/** Further study!
+	 * lowestCommonAncestor(node1, node2): find the lowest common ancestor
+	 * of two nodes in a binary tree. */
 
-  /** Further study!
-   * serialize(tree): serialize the BinaryTree object tree into a string. */
-
-  static serialize() {
-
-  }
-
-  /** Further study!
-   * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
-
-  static deserialize() {
-
-  }
-
-  /** Further study!
-   * lowestCommonAncestor(node1, node2): find the lowest common ancestor
-   * of two nodes in a binary tree. */
-
-  lowestCommonAncestor(node1, node2) {
-    
-  }
+	lowestCommonAncestor(node1, node2) {}
 }
 
 module.exports = { BinaryTree, BinaryTreeNode };
