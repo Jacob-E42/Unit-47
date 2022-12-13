ecb9576119eaf8a93c422cee9d106c4252896386
Jacob Eiferman <jacobeiferman@gmail.com>
2022-12-13 13:16:18 -0500

    implemented Graph removeVertex()

--- Graphs/graph.js
+++ Graphs/graph.js
@@ -31,10 +31,19 @@ class Graph {
 	}
 
 	// this function accepts two vertices and updates their adjacent values to remove the other vertex
-	removeEdge(v1, v2) {}
+	removeEdge(v1, v2) {
+		if (v1 && v2) {
+			v1.adjacent.delete(v2);
+			v2.adjacent.delete(v1);
+		}
+	}
 
 	// this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-	removeVertex(vertex) {}
+	removeVertex(vertex) {
+		if (vertex) {
+			this.nodes.delete(vertex);
+		}
+	}
 
 	// this function returns an array of Node values using DFS
 	depthFirstSearch(start) {}
