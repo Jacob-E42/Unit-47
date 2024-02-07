a5ca21f4661ec802860a4c47609c1191e8add310
Jacob Eiferman <jacobeiferman@gmail.com>
2024-02-07 13:57:29 -0500

    queue exercise

--- Stacks and Queues/queue.js
+++ Stacks and Queues/queue.js
@@ -1,14 +1,30 @@
 /** Node: node for a queue. */
 
+// class Node {
+// 	constructor(val) {
+// 		this.val = val;
+// 		this.next = null;
+// 	}
+// }
+
+/** Queue: chained-together nodes where you can
+ *  remove from the front or add to the back. */
+
+// class Queue {
+
+// Queues
+// Make a Queue class. It should include methods for enqueuing, dequeuing, peeking, and checking if the queue is empty.
+
+// Make it throw an error if you try to dequeue from an empty queue.
+
+// Stacks
+
 class Node {
 	constructor(val) {
 		this.val = val;
 		this.next = null;
 	}
 }
-
-/** Queue: chained-together nodes where you can
- *  remove from the front or add to the back. */
 
 class Queue {
 	constructor() {
@@ -17,47 +33,26 @@ class Queue {
 		this.size = 0;
 	}
 
-	/** enqueue(val): add new value to end of the queue. Returns undefined. */
-
 	enqueue(val) {
-		const node = new Node(val);
+		const newNode = new Node(val);
 		if (this.size === 0) {
-			this.first = node;
-			this.last = this.first;
+			this.first = newNode;
+		} else if (this.size === 1) {
+			this.first.next = newNode;
 		} else {
-			this.last.next = node;
-			this.last = node;
+			this.last.next = newNode;
 		}
+		this.last = newNode;
 		this.size++;
 	}
 
-	/** dequeue(): remove the node from the start of the queue
-	 * and return its value. Should throw an error if the queue is empty. */
-
 	dequeue() {
-		if (this.isEmpty()) throw new Error("Error: Queue is empty.");
-
-		const removed = this.first;
-		if (this.size === 1) {
-			this.first = null;
-			this.last = null;
-		} else {
-			this.first = this.first.next;
-		}
+		if (this.size === 0) throw new Error("There are no items to dequeue");
+		const val = this.first.val;
+		const secondNode = this.first.next;
+		this.first = secondNode || null;
 		this.size--;
-		return removed.val;
-	}
-
-	/** peek(): return the value of the first node in the queue. */
-
-	peek() {
-		return this.first.val;
-	}
-
-	/** isEmpty(): return true if the queue is empty, otherwise false */
-
-	isEmpty() {
-		return this.size === 0;
+		return val;
 	}
 }
 
